0d39a8180d30f86f672dc899391b34bca153659f
Nicola Bolzan <bolzan.nicola@gmail.com>
2023-05-31 22:53:57 +0200

    refactor: shorten day 1 2022

--- 2022/day-1/calorie_counting.py
+++ 2022/day-1/calorie_counting.py
@@ -5,34 +5,15 @@ from typing import List
 
 
 def calorie_count(values: List[str]) -> int:
-    a, b = 0, 0
-
-    for line in values:
-        if line != '':
-            b += int(line)
-        else:
-            if b > a:
-                a = b
-            b = 0
-    return a
-
-
-def top_3_calorie_count(values: List[str]) -> int:
-    a = [0]
-    i = 0
-    for line in values:
-        if line != '':
-            a[i] += int(line)
-        else:
-            a.append(0)
-            i += 1
-    a.sort()
-    return sum(a[-3:])
+    cal = []
+    for value in values:
+        cal.append(sum(map(int, value.splitlines())))
+    return sorted(cal)
 
 
 if __name__ == '__main__':
     with open('input-1.txt', 'r') as f:
-        input1 = f.read().split(sep='\n')
+        input1 = f.read().split(sep='\n\n')
 
-    print(f'Answer 1: {calorie_count(input1)}')
-    print(f'Answer 2: {top_3_calorie_count(input1)}')
+    print(f'Answer 1: {calorie_count(values=input1)[-1]}')
+    print(f'Answer 2: {sum(calorie_count(values=input1)[-3:])}')
